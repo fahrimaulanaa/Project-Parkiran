280fbf0281703bab012179830a5f91b10d696c36
Fahri Maulana <88439222+fahrimaulanaa@users.noreply.github.com>
2022-01-24 10:55:26 +0700

    Add files via upload

---
+++ tumbuhan.java
@@ -0,0 +1,26 @@
+public class tumbuhan {
+
+    private static String tumbuhan(){
+        String pohon = "jati";
+        return pohon;
+    }
+
+    public static String tumbuhan1(){
+        String buah = "Apel";
+        return buah;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(tumbuhan());
+        System.out.println(tumbuhan1());
+        buah();
+    }
+
+    private static void buah(){
+        String buahan = "Mangga";
+        System.out.println(buahan);
+
+        String buahan2 = "Pelem";
+        System.out.println(buahan2);
+    }
+}
